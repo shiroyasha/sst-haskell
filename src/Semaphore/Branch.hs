65cad26e1bb5ec142dda07ffd4bad90314baded9
Igor Sarcevic <igisar@gmail.com>
2015-04-28 01:10:59 +0200

    Show branch status

--- src/Semaphore/Branch.hs
+++ src/Semaphore/Branch.hs
@@ -8,7 +8,9 @@ module Semaphore.Branch ( Branch (..)
 import Data.Aeson as Json
 import GHC.Generics
 
-data Branch = Branch { branch_name :: !String } deriving (Show, Generic)
+data Branch = Branch { branch_name :: String
+                     , result      :: Maybe String
+                     } deriving (Show, Generic)
 
 
 instance Json.FromJSON Branch
@@ -16,7 +18,9 @@ instance Json.ToJSON Branch
 
 
 showBranch :: Branch -> String
-showBranch branch = "  - " ++ branch_name branch
+showBranch branch = "  - " ++ branch_name branch ++ branchStatus (result branch)
+  where branchStatus (Just status) = " :: " ++ status
+        branchStatus Nothing       = " :: Not Yet Built"
 
 
 showBranches :: [Branch] -> String
