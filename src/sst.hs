753da6056b562b3614f1d1dec8354d5cd9faa6c5
Igor Sarcevic <igisar@gmail.com>
2015-05-02 14:53:53 +0200

    Catch network error

--- src/sst.hs
+++ src/sst.hs
@@ -8,6 +8,8 @@ import qualified Data.Text.IO as TIO
 import Semaphore.Api
 import Semaphore.Project
 import Semaphore.Config
+import Network.HTTP.Conduit
+import Control.Exception
 
 cantFindApiToken :: Text
 cantFindApiToken = intercalate "\n" [
@@ -17,21 +19,24 @@ cantFindApiToken = intercalate "\n" [
   "  echo '<api_token>' > ~/.sst/api_token"
   ]
 
+
 connectionError :: Text
 connectionError = intercalate "\n" [
   "The connection to Semaphore's API failed.",
   "Please make sure you have a working internet connection."
   ]
 
+networkExceptionHandler :: HttpException -> IO (Either String a)
+networkExceptionHandler _ = return $ Left (unpack connectionError)
 
 showProjectTree :: String -> IO ()
 showProjectTree apiToken = do
   apiDomain <- loadApiDomain
 
-  projects  <- Semaphore.Api.getProjects apiDomain apiToken
+  projects  <- Semaphore.Api.getProjects apiDomain apiToken `catch` networkExceptionHandler
 
   case projects of
-    Left err -> TIO.putStrLn connectionError
+    Left err -> putStrLn err
     Right pr -> putStrLn $ showProjects pr
 
 
